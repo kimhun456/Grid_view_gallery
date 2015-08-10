2139efea143c27bd30e3cdbcc6922c3d6d4653a6
HyunJae <kimhun456@gmail.com>
2015-08-10 23:52:51 +0900

    first commit

---
+++ app/src/main/java/secmem/org/util/AppConstant.java
@@ -0,0 +1,23 @@
+package secmem.org.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by HyunJae on 2015-08-10.
+ */
+public class AppConstant {
+
+    // Number of columns of Grid View
+    public static final int NUM_OF_COLUMNS = 3;
+
+    // Gridview image padding
+    public static final int GRID_PADDING = 8; // in dp
+
+    // SD card image directory
+    public static final String PHOTO_ALBUM = "Beenzido";
+
+    // supported file formats
+    public static final List<String> FILE_EXTN = Arrays.asList("jpg", "jpeg",
+            "png");
+}
